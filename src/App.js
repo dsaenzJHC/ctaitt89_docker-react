bbe4ee641ad2040ca295f05ded08e3f1f1d601f0
dsaenzJHC <dsaenz@jhctechnology.com>
2018-12-09 15:25:01 -0500

    changed text for app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            HI THERE
+             I WAS CHANGED ON THE FEATURE BRANCH HI THERE
           </p>
           <a
             className="App-link"
